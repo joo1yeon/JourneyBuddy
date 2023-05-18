2bb1cf03170a81717cd2557be411b8744efd66af
진병욱 <quddnr678@naver.com>
2023-05-18 15:14:38 +0900

    공지사항 최종

--- enjoytrip/src/components/notice/NoticeList.vue
+++ enjoytrip/src/components/notice/NoticeList.vue
@@ -1,22 +1,26 @@
 <template>
   <b-container id="list">
-    <h2>공지사항 게시판</h2>
-    <b-button variant="success" type="button" :to="{ name: 'noticewrite' }">작성하기</b-button>
-    <!-- <b-table hover :items="tableData" @row-clicked="noticeView" :fields="fields" v-if="noticeList.length && !mainNotice">
-      <template #cell(userId)="data">
-        <div v-if="data.item.userId === 'ssafy'">1</div>
-      </template>
-    </b-table> -->
-    <b-table hover :items="tableData" @row-clicked="noticeView" :fields="fields" v-if="noticeList.length" :top-row="topRowContent">
-      <template #cell(userId)="data">
-        <div v-if="data.item.userId === 'ssafy'">2</div>
-      </template>
-    </b-table>
-    <!-- <b-table hover :items="tableData" @row-clicked="noticeView" :fields="fields" v-if="(noticeList.length = 0)">
-      <template #cell(userId)="data">
-        <div v-if="data.item.userId === 'ssafy'">3</div>
-      </template>
-    </b-table> -->
+    <b-alert show><h2>공지사항</h2></b-alert>
+    <div class="d-flex mb-3">
+      <b-button variant="success" type="button" :to="{ name: 'noticewrite' }">작성하기</b-button>
+    </div>
+    <div v-if="!isNotice && noticeList">
+      <b-table hover :items="tableData" @row-clicked="noticeView" :fields="fields">
+        <template #cell(userId)="data">
+          <div v-if="data.item.userId === 'ssafy'">관리자</div>
+        </template>
+      </b-table>
+    </div>
+    <div v-else-if="isNotice && noticeList">
+      <b-table hover :items="tableData" @row-clicked="noticeView" :fields="fields" :top-row="topRowContent">
+        <template #cell(userId)="data">
+          <div v-if="data.item.userId === 'ssafy'">관리자</div>
+        </template>
+      </b-table>
+    </div>
+    <div v-else>
+      <h2>공지사항이 존재하지 않습니다</h2>
+    </div>
   </b-container>
 </template>
 
@@ -48,19 +52,19 @@ export default {
       ],
       noticeList: [],
       mainNotice: null,
+      isNotice: false,
     };
   },
   computed: {
     tableData() {
       if (this.mainNotice) {
         return [this.mainNotice, ...this.noticeList];
-        // return [...this.noticeList];
       } else {
         return this.noticeList;
       }
     },
     topRowContent() {
-      if (this.mainNotice) {
+      if (this.isNotice) {
         return [this.mainNotice];
       } else {
         return null;
@@ -70,9 +74,15 @@ export default {
   created() {
     http.get("/notice/list").then((response) => {
       this.noticeList = response.data;
-      this.mainNotice = { ...this.noticeList.find((notice) => notice.mainNotice === "true") };
-      if (this.mainNotice != null || this.mainNotice.length > 0 || !this.mainNotice) {
+      this.mainNotice !== undefined;
+      if (this.noticeList) {
+        this.mainNotice = this.noticeList.find((notice) => notice.mainNotice === "true");
+      }
+      if (this.mainNotice !== undefined && this.noticeList) {
         this.mainNotice._rowVariant = "danger";
+        this.isNotice = true;
+      } else {
+        this.isNotice = false;
       }
     });
   },
--- enjoytrip/src/components/notice/NoticeModify.vue
+++ enjoytrip/src/components/notice/NoticeModify.vue
@@ -1,32 +1,30 @@
 <template>
   <b-container fluid="lg" id="modify">
-    <h2>공지사항 수정하기</h2>
+    <b-alert show class="text-left"
+      ><h2>{{ form.noticeNo }}. {{ form.subject }} (수정)</h2></b-alert
+    >
     <b-form @submit="onSubmit">
-      <b-form-group id="input-group-1" label="제목" label-for="input-1">
+      <b-form-group id="input-group-1" label="제목" label-for="input-1" label-align="left">
         <b-form-input id="input-1" v-model="form.subject" type="text" placeholder="제목을 입력하세요" required></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="내용" label-for="input-2">
+      <b-form-group id="input-group-2" label="내용" label-for="input-2" label-align="left">
         <b-form-textarea id="textarea" v-model="form.content" placeholder="내용을 입력하세요" rows="3" max-rows="6" required></b-form-textarea>
       </b-form-group>
-      <b-form-group id="input-group-1" label="조회수" label-for="input-3">
+      <b-form-group id="input-group-1" label="조회수" label-for="input-3" label-align="left">
         <b-form-input id="input-3" v-model="form.hit" type="number" required readonly></b-form-input>
       </b-form-group>
-      <b-form-group id="input-group-1" label="등록 시간" label-for="input-4">
+      <b-form-group id="input-group-1" label="등록 시간" label-for="input-4" label-align="left">
         <b-form-input id="input-4" v-model="form.registerTime" type="text" required readonly></b-form-input>
       </b-form-group>
 
       <!-- 메인 공지 작성 여부 -->
-      <b-form-group id="input-group-4">
-        <b-form-checkbox v-model="form.mainNotice">메인 공지사항 등록</b-form-checkbox>
-      </b-form-group>
+      <b-form-checkbox v-model="form.mainNotice" id="input-5" class="text-left">메인 공지사항 등록</b-form-checkbox>
 
-      <b-button type="submit" variant="primary">수정완료</b-button>
+      <!-- 버튼 -->
+      <b-button type="submit" variant="primary" class="mr-4">수정완료</b-button>
       <b-button type="button" @click="goList" variant="info">리스트</b-button>
     </b-form>
-    <!-- <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card> -->
   </b-container>
 </template>
 
