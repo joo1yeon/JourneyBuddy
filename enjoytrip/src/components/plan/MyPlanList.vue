d3a8a358f34d73b348baf4344169a0212f7911eb
Byeonguk Jin <quddnr678@naver.com>
2023-05-23 02:25:54 +0900

    나의 여행 계획 초안

--- enjoytrip/src/components/plan/MyPlanList.vue
+++ enjoytrip/src/components/plan/MyPlanList.vue
@@ -1,17 +1,47 @@
 <template>
-  <b-container id="list"></b-container>
+  <b-container id="list">
+    <b-alert variant="danger" show><h2>나의 여행 계획</h2></b-alert>
+    <div v-if="planList">
+      <b-table hover :items="planList" @row-clicked="planView" :fields="fields"></b-table>
+    </div>
+    <div v-else>
+      <h2>나의 여행 계획이 존재하지 않습니다</h2>
+    </div>
+  </b-container>
 </template>
 
 <script>
+// import http from "@/api/http";
 export default {
   name: "MyPlanList",
   components: {},
   data() {
     return {
-      message: "",
+      fields: [
+        {
+          key: "planNum",
+          label: "계획 번호",
+          sortable: true,
+        },
+        {
+          key: "contentId",
+          label: "장소 명",
+        },
+        {
+          key: "time",
+          label: "시간",
+          sortable: true,
+        },
+      ],
+      planList: [],
     };
   },
-  created() {},
+  created() {
+    this.planList = null;
+    // http.get("/plan/list/ssafy").then((response) => {
+    //   this.planList = response.data;
+    // });
+  },
   methods: {},
 };
 </script>
